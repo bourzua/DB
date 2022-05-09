b876f7a17ad968e0153cf15f127c314bf067da6f
jua <bourzua@naver.com>
2022-05-09 21:45:43 +0900

    jdbc-등록

--- 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -3,11 +3,8 @@ package hello.jdbc.repository;
 import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * JDBC - DriverManager 사용
@@ -15,25 +12,57 @@ import java.sql.SQLException;
 @Slf4j
 public class MemberRepositoryV0 {
 
-    public Member save(Member member) {
+    public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values (?, ?)";
 
         Connection con = null;
         PreparedStatement pstmt = null;
 
-
-        con = getConnection();
         try {
+            con = getConnection();
             pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+        private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.error("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();   // SQLException
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
         }
 
 
-    }
-
-    @NotNull
-    private Connection getConnection() {
-        return DBConnectionUtil.getConnection();
-    }
+        private Connection getConnection() {
+            return DBConnectionUtil.getConnection();
+        }
 }
