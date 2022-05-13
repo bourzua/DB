3b95b0f22a87b75092ef544fddcee0fa4bfe6d19
jua <bourzua@naver.com>
2022-05-13 16:56:02 +0900

    transaction적용1

--- 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -13,5 +13,16 @@ public class MemberServiceV1 {
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
     }
 }
