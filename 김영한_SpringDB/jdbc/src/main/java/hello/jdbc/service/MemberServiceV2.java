3a69c43a8a1c7f0393d7df5cc31972c90a722888
jua <bourzua@naver.com>
2022-05-20 17:26:46 +0900

    충돌 수정

--- 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
+++ 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -1,10 +1,9 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV1;
+import hello.jdbc.repository.MemberRepositoryV2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -19,19 +18,13 @@ import java.sql.SQLException;
 public class MemberServiceV2 {
 
     private final DataSource dataSource;
-    private final MemberRepositoryV1 memberRepository;
+    private final MemberRepositoryV2 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
         Connection con = dataSource.getConnection();
         try {
             con.setAutoCommit(false); // 트랜잭션 시작
-            // 비즈니스 로직
-            Member fromMember = memberRepository.findById(fromId);
-            Member toMember = memberRepository.findById(toId);
-
-            memberRepository.update(fromId, fromMember.getMoney() - money);
-            validation(toMember);
-            memberRepository.update(toId, toMember.getMoney() + money);
+            bizLogic(con, fromId, toId, money);
             con.commit(); // 성공시 커밋
         } catch (Exception e) {
             con.rollback(); // 실패시 롤백
@@ -39,6 +32,16 @@ public class MemberServiceV2 {
         } finally {
             release(con);
         }
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        // 비즈니스 로직
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(con, toId, toMember.getMoney() + money);
     }
 
     private void validation(Member toMember) {
