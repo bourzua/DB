bb8b25210485943d19fc36f730808216418b1a1d
jua <bourzua@naver.com>
2022-06-01 12:24:20 +0900

    release제거

--- 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ 김영한_SpringDB/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -3,37 +3,26 @@ package hello.jdbc.service;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 템플릿
+ * 트랜잭션 - @Transactional AOP
  */
 
 @Slf4j
 public class MemberServiceV3_3 {
-//    private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate txTemplate;
+
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_3(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-
-        txTemplate.executeWithoutResult((status) -> {
-            // 비즈니스 로직
-            try {
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
@@ -49,17 +38,6 @@ public class MemberServiceV3_3 {
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생");
-        }
-    }
-
-    private void release(Connection con) {
-        if (con != null) {
-            try {
-                con.setAutoCommit(true); // 커넥션 풀 고려
-                con.close();
-            } catch (Exception e) {
-                log.info("error", e);
-            }
         }
     }
 }
